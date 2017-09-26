fb147e788c236c9d95ba36e7f8dfce18610bec6c
Xavier Bruni <b.xavier@hotmail.fr>
2017-09-26 08:45:13 +0200

    indentation

--- webserver/main.c
+++ webserver/main.c
@@ -1,8 +1,7 @@
 #include "socket.h"
-int main(void)
-{
-  int port = 8080;
-  void initialiser_signaux(void);
-  creer_serveur(port);
-  return 0;
+int main(void){
+	int port = 8080;
+	void initialiser_signaux(void);
+	creer_serveur(port);
+	return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -2,66 +2,61 @@
 #define __SOCKET_H__
 #include "socket.h"
 
-int creer_serveur(int port)
-{
-  int socket_serveur = -1;
-  int socket_client = -1;
-  const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
-  int optval = 1;
-  
-  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-  
-  if(socket_serveur == -1)
-    {
-      perror("socket_serveur");
-      return -1;
-    }
+int creer_serveur(int port){
 
-  struct sockaddr_in saddr;
-  saddr.sin_family = AF_INET;
-  saddr.sin_port = htons(port);
-  saddr.sin_addr.s_addr = INADDR_ANY;
+	int socket_serveur = -1;
+	int socket_client = -1;
+	const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+	int optval = 1;
 
-  if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
-    perror("Impossible de modifier l'option SO_REUSEADDR\n");
-  
-  if(bind(socket_serveur, (struct sockaddr * )&saddr, sizeof(saddr)) == 1)
-  {
-    perror("bind socket_serveur");
-    return -1;
-  }
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
-  if(listen(socket_serveur, 10) == -1)
-    {
-      perror("listen socket_serveur");
-      return -1;
-    }
-
- 
-
-  while(1)
-    {
-      printf("Tentative connexion...\n");
-      socket_client = accept(socket_serveur, NULL, NULL);
-      
-      if(socket_client == -1)
-	{
-	  perror("accept");
-	  return -1;
+	if(socket_serveur == -1){
+		perror("socket_serveur");
+		return -1;
 	}
-      printf("Connexion OK\n");
-      sleep(1);
-      write(socket_client, message_bienvenue, strlen(message_bienvenue));
-    }
-  return 0;
+
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(port);
+	saddr.sin_addr.s_addr = INADDR_ANY;
+
+	if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1)
+		perror("Impossible de modifier l'option SO_REUSEADDR\n");
+
+	if(bind(socket_serveur, (struct sockaddr * )&saddr, sizeof(saddr)) == 1){
+		perror("bind socket_serveur");
+		return -1;
+	}
+
+	if(listen(socket_serveur, 10) == -1){
+		perror("listen socket_serveur");
+		return -1;
+	}
+
+
+	while(1){
+		printf("Tentative connexion...\n");
+		socket_client = accept(socket_serveur, NULL, NULL);
+	
+		if(socket_client == -1){
+			perror("accept");
+			return -1;
+		}
+	  	printf("Connexion OK\n");
+	  	sleep(1);
+	  	write(socket_client, message_bienvenue, strlen(message_bienvenue));
+	}
+
+	return 0;
+
 }
 
-void initialiser_signaux(void)
-{
-  if(signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-     {
-       perror("signal");
-       exit(-1);
-     }
+void initialiser_signaux(void){
+	if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
+		perror("signal");
+		exit(-1);
+	}
 }
+
 #endif
