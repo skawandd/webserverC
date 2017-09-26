75d7d4023e732d3e23cf2ab84a77ca3142e23e0a
Xavier Bruni <b.xavier@hotmail.fr>
2017-09-26 08:28:15 +0200

    header

---
+++ webserver/socket.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+extern int creer_serveur(int port);
+
